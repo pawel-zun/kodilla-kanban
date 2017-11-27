0bc3fddc66f1c6966a84c6c414a636f3ab8dd2d7
Pawel Zun <pzun90@gmail.com>
2017-11-27 01:32:12 +0100

    finish js of multiple boards - some css issues are still to resolve

--- script.js
+++ script.js
@@ -9,6 +9,63 @@ $(function() {
 		return str;
 	}
 
+	var boardsContainer = {
+		$element: $('#boards-container'),
+		addBoard: function(board) {
+			this.$element.append(board.$element);
+		}
+	};
+
+	 $('.create-board').click(function() {
+  	var name = prompt('Enter a board name');
+  	if ((!(name == false)) && (!(name == null))) {
+  	var board = new Board(name);
+  	boardsContainer.addBoard(board);
+	  } else if (name == false) {
+	  	alert('Error! New board must be named.');
+	  };
+  });
+
+	function Board(name) {
+		var self = this;
+		this.id = randomString();
+		this.name = name;
+		this.$element = createBoard();
+
+		function createBoard() {
+			var $board = $('<div>').addClass('board');
+			var $boardTitle = $('<h1>').text(self.name);
+			var $boardAddColumn = $('<button>').addClass('add-card').text('Add a column');
+			var $boardDelete = $('<button>').addClass('btn-delete').text('Delete board');
+			var $columnContainer = $('<div>').addClass('column-container');
+
+
+			$boardDelete.click(function() {
+				if (confirm('Are you sure you want to delete this board?')) {
+					self.removeBoard();
+				}
+			});
+
+			$boardAddColumn.click(function() {
+				self.addColumn(new Column(prompt('Enter the name of the column')));
+			});
+
+			$board.append($boardTitle).append($boardAddColumn).append($boardDelete).append($columnContainer);
+
+			return $board;
+		}
+	}
+
+	Board.prototype = {
+		addColumn: function(column) {
+			this.$element.children('.column-container').append(column.$element);
+			initSortable();
+		},
+		removeBoard: function() {
+			this.$element.remove();
+		}
+	};
+
 	function Column(name) {
 		var self = this;
 		this.id = randomString();
@@ -16,23 +73,27 @@ $(function() {
 		this.$element = createColumn();
 
 		function createColumn() {
-			var $column = $('<div>').addClass('column');
-			var $columnTitle = $('<h2>').addClass('column-title').text(self.name);
-			var $columnCardList = $('<ul>').addClass('column-card-list');
-			var $columnDelete = $('<button>').addClass('btn-delete').text('x');
-			var $columnAddCard = $('<button>').addClass('add-card').text('Add a card');
+  		if ((!(self.name == false)) && (!(self.name == null))) {
+				var $column = $('<div>').addClass('column');
+				var $columnTitle = $('<h2>').addClass('column-title').text(self.name);
+				var $columnCardList = $('<ul>').addClass('column-card-list');
+				var $columnDelete = $('<button>').addClass('btn-delete').text('Delete column');
+				var $columnAddCard = $('<button>').addClass('add-card').text('Add a card');
 
-			$columnDelete.click(function() {
-				self.removeColumn();
-			});
+				$columnDelete.click(function() {
+					self.removeColumn();
+				});
 
-			$columnAddCard.click(function() {
-				self.addCard(new Card(prompt('Enter the name of the card')));
-			});
+				$columnAddCard.click(function() {
+					self.addCard(new Card(prompt('Enter the name of the card')));
+				});
 
-			$column.append($columnTitle).append($columnDelete).append($columnAddCard).append($columnCardList);
+				$column.append($columnTitle).append($columnAddCard).append($columnDelete).append($columnCardList);
 
-			return $column;
+				return $column;
+			} else if (name == false) {
+		  	alert('Error! New column must be named.');
+		  };
 		};
 	};
 
@@ -53,17 +114,17 @@ $(function() {
 
 		function createCard() {
 			if ((!(self.description == false)) && (!(self.description == null))) {
-			var $card = $('<li>').addClass('card');
-			var $cardDescription = $('<p>').addClass('card-description').text(self.description);
-			var $cardDelete = $('<button>').addClass('btn-delete').text('x');
+				var $card = $('<li>').addClass('card');
+				var $cardDescription = $('<p>').addClass('card-description').text(self.description);
+				var $cardDelete = $('<button>').addClass('btn-delete').text('x');
 
-			$cardDelete.click(function() {
-				self.removeCard();
-			});
+				$cardDelete.click(function() {
+					self.removeCard();
+				});
 
-			$card.append($cardDelete).append($cardDescription);
+				$card.append($cardDelete).append($cardDescription);
 
-			return $card;
+				return $card;
 			} else if (self.description == false) {
 				alert('Please write down task description');
 			};
@@ -76,15 +137,6 @@ $(function() {
 		}
 	};
 
-	var board = {
-		name: 'Kanban Board',
-		$element: $('#board .column-container'),
-		addColumn: function(column) {
-			this.$element.append(column.$element);
-			initSortable();
-		}
-	};
-
 	function initSortable() {
   	$('.column-card-list').sortable({
   		connectWith: '.column-card-list',
@@ -92,28 +144,40 @@ $(function() {
   	}).disableSelection();
   };
 
-  $('.create-column').click(function() {
-  	var name = prompt('Enter a column name');
-  	if ((!(name == false)) && (!(name == null))) {
-  	var column = new Column(name);
-  	board.addColumn(column);
-  } else if (name == false) {
-  	alert('Error! New column must be named.');
-  };
-  });
+  var board1 = new Board('Kanban Board');
+  boardsContainer.addBoard(board1);
 
   var todoColumn = new Column('To do');
   var doingColumn = new Column('Doing');
   var doneColumn = new Column('Done');
 
-  board.addColumn(todoColumn);
-  board.addColumn(doingColumn);
-  board.addColumn(doneColumn);
+  board1.addColumn(todoColumn);
+  board1.addColumn(doingColumn);
+  board1.addColumn(doneColumn);
 
   var card1 = new Card('New task');
   var card2 = new Card('Create kanban boards');
+  var card3 = new Card('Create multiple boards feature');
 
   todoColumn.addCard(card1);
   doingColumn.addCard(card2);
+  doneColumn.addCard(card3);
+
+  var board2 = new Board('Skyrim Quests');
+  boardsContainer.addBoard(board2);
+
+  var column1 = new Column('To do');
+  var column2 = new Column('Done');
+  var column3 = new Column('FUS RO DAH');
+
+  board2.addColumn(column1);
+  board2.addColumn(column2);
+  board2.addColumn(column3);
+
+  var card21 = new Card('All quests');
+  var card22 = new Card('Start playing expansions');
+
+  column1.addCard(card22);
+  column2.addCard(card21);
 
 })
